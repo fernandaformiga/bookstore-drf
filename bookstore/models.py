9a846f4235e5b07b1ef114697269bc80cb8bc1d2
fernandaformiga <ferformigas@gmail.com>
2021-06-15 22:32:06 -0300

    Create models

--- bookstore/models.py
+++ bookstore/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
-# Create your models here.
+class Author(models.Model):
+    name = models.CharField(max_length=50)
+    country = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.name
+
+class Book(models.Model):
+    STOCK = (
+        ('A', 'available'),
+        ('U', 'unavailable'),
+        ('F', 'few units left')
+    )
+    quantity = models.IntegerField()
+    year = models.IntegerField()
+    genre = models.CharField(max_length=50)
+    author = models.ManyToManyField(Author)
+
+    def __str__(self):
+        return self.descricao
